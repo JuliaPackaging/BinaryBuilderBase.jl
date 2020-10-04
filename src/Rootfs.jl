26a3194d2cb3717a10c64073f60b87ad2abcd55d
Mosè Giordano <giordano@users.noreply.github.com>
2020-10-04 21:12:26 +0100

    More flexible skipping of C++ string ABIs expansion (#46)
    Let the user pass a custom function to selectively skip expansion

--- src/Rootfs.jl
+++ src/Rootfs.jl
@@ -682,7 +682,7 @@ function expand_gfortran_versions(ps::Vector{<:Platform})
 end
 
 """
-    expand_cxxstring_abis(p::AbstractPlatform; skip_freebsd_macos::Bool=true)
+    expand_cxxstring_abis(p::AbstractPlatform; skip=Sys.isbsd)
 
 Given a `Platform`, returns an array of `Platforms` with a spread of identical
 entries with the exception of the `cxxstring_abi` tag within the `Platform`
@@ -690,13 +690,14 @@ object.  This is used to take, for example, a list of supported platforms and
 expand them to include multiple GCC versions for the purposes of ABI matching.
 
 If the given `Platform` already specifies a `cxxstring_abi` (as opposed to
-`nothing`) only that `Platform` is returned.  If `skip_freebsd_macos` is
-`true`, FreeBSD and MacOS platforms are not expanded, due to their lack of a
+`nothing`) only that `Platform` is returned.  If `skip` is a function for which
+`skip(platform)` evaluates to `true`, the given platform is not expanded.  By
+default FreeBSD and macOS platforms are skipped, due to their lack of a
 dependence on `libstdc++` and not needing this compatibility shim.
 """
-function expand_cxxstring_abis(platform::AbstractPlatform; skip_freebsd_macos::Bool=true)
+function expand_cxxstring_abis(platform::AbstractPlatform; skip=Sys.isbsd)
     # If this platform cannot/should not be expanded, then exit out fast here.
-    if cxxstring_abi(platform) !== nothing || (skip_freebsd_macos && Sys.isbsd(platform))
+    if cxxstring_abi(platform) !== nothing || skip(platform)
         return [platform]
     end
 
