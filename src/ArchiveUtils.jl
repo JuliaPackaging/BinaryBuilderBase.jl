914e6f569ed622842a5dfa7625c5a29ca8887d82
Elliot Saba <staticfloat@gmail.com>
2020-10-16 16:09:22 -0700

    Use `curl` temporarily until `Downloads` fixes make their way into `1.6`

--- src/ArchiveUtils.jl
+++ src/ArchiveUtils.jl
@@ -127,6 +127,13 @@ function verify(path::AbstractString, hash::AbstractString; hash_path::AbstractS
 end
 
 function download_verify(url, hash, path)
-    Downloads.download(url, path)
+    # Temporarily shell out to `curl` to download, until `Downloads` bugs are squashed
+    try
+        run(`curl -C - -s -\# -f -L $(url) -o $(path)`)
+    catch e
+        # Downloads throws an ErrorException, so we'll do the same
+        error("download failed: $(e)")
+    end
+    #Downloads.download(url, path)
     verify(path, hash) || error("Verification failed")
 end
