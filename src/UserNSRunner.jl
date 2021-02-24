7d3f3d00446a9bc0ed9a804b008d1c1f2e433e9f
Michael Schlottke-Lakemper <michael@sloede.com>
2021-02-24 06:25:19 +0100

    Mount existing `/etc/resolv.conf` in `UserNSRunner` if it has nameserver settings (#117)

--- src/UserNSRunner.jl
+++ src/UserNSRunner.jl
@@ -62,6 +62,21 @@ function UserNSRunner(workspace_root::String;
         push!(workspaces, ccache_dir() => "/root/.ccache")
     end
 
+    # If we are on a system that uses `/etc/resolv.conf` as a resolver
+    # configuration file (mainly *BSD and Linux), check if a nameserver is
+    # specified and if yes, mount the existing `/etc/resolv.conf` to make use
+    # of system-specific nameserver settings
+    if (Sys.isbsd() || Sys.islinux()) && isfile("/etc/resolv.conf")
+        if any(startswith(line, "nameserver") for line in eachline("/etc/resolv.conf"))
+            # We copy the contents of `/etc/resolv.conf` to a temporary file and mount that one,
+            # such that `/etc/resolv.conf` is editable inside the container
+            tmppath, tmpio = mktemp()
+            write(tmpio, read("/etc/resolv.conf", String))
+            flush(tmpio) # required as otherwise `tmppath` remains empty
+            push!(workspaces, tmppath => "/etc/resolv.conf")
+        end
+    end
+
     if isnothing(shards)
         # Choose the shards we're going to mount
         shards = choose_shards(platform; extract_kwargs(kwargs, (:preferred_gcc_version,:preferred_llvm_version,:bootstrap_list,:compilers))...)
