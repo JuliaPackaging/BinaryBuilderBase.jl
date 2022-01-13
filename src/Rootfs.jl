73b875d35b9c4fd1d8404b73f371cf4caf3a9776
Venkateshprasad <vensadk@gmail.com>
2022-01-13 14:40:18 +0530

    Make BBBase relocatable on sysimages
    - create a const artifacts_toml with @path
    - use envcache to determine the version instead of parsing the project.toml: Using @path here doesn't work as
          - a: Project.toml which at DIR*..
          - b: the entire `src` would be stored in the Path object (and not Project.toml)
    - While creating storage_cache check if "deps_path" is valid else create one at dev/BBBase.jl/deps: Even here using @path directly is of no use as you are creating a new folder and don't intend any file to survive relocation
    So now it checks if the path is valid; ow creates one at .julia/dev
    Leaving this storage_cache untouched will result in creating a folder identical to original-depot-path failing all the RelocatableFolder's `getpath` efforts

--- src/Rootfs.jl
+++ src/Rootfs.jl
@@ -94,7 +94,6 @@ end
 const ALL_SHARDS = Ref{Union{Vector{CompilerShard},Nothing}}(nothing)
 function all_compiler_shards()::Vector{CompilerShard}
     if ALL_SHARDS[] === nothing
-        artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
         artifact_dict = load_artifacts_toml(artifacts_toml)
 
         ALL_SHARDS[] = CompilerShard[]
@@ -124,7 +123,6 @@ function all_compiler_shards()::Vector{CompilerShard}
 end
 
 function shard_source_artifact_hash(cs::CompilerShard)
-    artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
     name = artifact_name(cs)
     hash = artifact_hash(
         name,
@@ -179,7 +177,6 @@ function mount_path(cs::CompilerShard, build_prefix::AbstractString)
         return joinpath(build_prefix, ".mounts", artifact_name(cs))
     else
         name = artifact_name(cs)
-        artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
         hash = artifact_hash(name, artifacts_toml; platform=cs.host)
         if hash === nothing
             error("Unable to find artifact $(name) within $(artifacts_toml)")
@@ -254,7 +251,6 @@ function mount(cs::CompilerShard, build_prefix::AbstractString; verbose::Bool = 
     end
 
     # Ensure this artifact is on-disk; hard to mount it if it's not installed
-    artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
     ensure_artifact_installed(artifact_name(cs), artifacts_toml; platform=cs.host, verbose=true)
 
     # Easy out if we're not Linux with a UserNSRunner trying to use a .squashfs
@@ -345,7 +341,6 @@ function macos_sdk_already_installed()
     css = all_compiler_shards()
     macos_artifact_names = artifact_name.(filter(cs -> cs.target !== nothing && Sys.isapple(cs.target::Platform), css))
 
-    artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
     macos_artifact_hashes = artifact_hash.(macos_artifact_names, artifacts_toml; platform=default_host_platform)
 
     # Return `true` if _any_ of these artifacts exist on-disk:
@@ -913,7 +908,6 @@ happens, you don't need an internet connection to build your precious, precious
 binaries.
 """
 function download_all_artifacts(; verbose::Bool = false)
-    artifacts_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
     ensure_all_artifacts_installed(
         artifacts_toml;
         include_lazy=true,
