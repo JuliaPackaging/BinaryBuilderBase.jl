622275abd5d103cce50397b08bd3550405d583a8
Mosè Giordano <giordano@users.noreply.github.com>
2022-04-14 22:52:22 +0200

    [Rootfs] Add LLVMBootstrap 13 (#236)
    * [Rootfs] Add LLVMBootstrap 13
    
    * [Platforms] Improve compiler flags for Apple M1
    
    * [Rootfs] Automatically collect versions of LLVM
    
    Until we don't need to specify extra ABI details, we can automatically find the
    available builds

--- test/platforms.jl
+++ test/platforms.jl
@@ -96,7 +96,7 @@ using BinaryBuilderBase: get_march_flags, get_all_arch_names, get_all_march_name
 
     # Test one that is different between gcc and clang
     @test get_march_flags("aarch64", "apple_m1", "gcc") == ["-march=armv8.5-a+aes+sha2+sha3+fp16fml+fp16+rcpc+dotprod", "-mtune=cortex-a76"]
-    @test get_march_flags("aarch64", "apple_m1", "clang") == ["-march=armv8.5-a+aes+sha2+sha3+fp16fml+fp16+rcpc+dotprod", "-mtune=apple-a12"]
+    @test get_march_flags("aarch64", "apple_m1", "clang") == ["-mcpu=apple-m1"]
 
     for compiler in ("gcc", "clang")
         # Make sure we get the right base microarchitecture for all compilers
