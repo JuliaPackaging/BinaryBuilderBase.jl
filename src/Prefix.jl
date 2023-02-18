907fe3f57dc9c8b3a9268cc3d6cd72ccd5f1d7a9
kittisopikulm <kittisopikulm@janelia.hhmi.org>
2023-02-17 20:36:30 -0500

    Optimization: Only extract the pkg tarball of interest

--- src/Prefix.jl
+++ src/Prefix.jl
@@ -339,11 +339,12 @@ function setup(source::SetupSource{ArchiveSource}, targetdir, verbose; tar_flags
             if verbose
                 @info "Extracting conda package $(basename(source.path))..."
             end
-            # First unzip the .conda file
-            run(`unzip -q $(source.path)`)
             # The .conda file contains an archive called pkg-*.tar.zst
             # Replace initial hash with pkg, and change the file extension to obtain the name
             pkg_name = replace(basename(source.path), r"^[a-z0-9]{64}-" => "pkg-", ".conda" => ".tar.zst")
+            # First unzip the pkg tarball from .conda file
+            run(`unzip -q $(source.path) $pkg_name`)
+            # Second untar the pkg tarball
             pkg_source = SetupSource{ArchiveSource}(joinpath(targetdir, pkg_name), source.hash, source.target)
             # Run setup again to untar the pkg binaries
             setup(pkg_source, targetdir, verbose; tar_flags = tar_flags)
