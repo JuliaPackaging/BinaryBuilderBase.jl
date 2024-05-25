5ae424b9868f852b225505e8953b844cabe5b480
Pere Mato <peremato@users.noreply.github.com>
2024-05-25 15:51:27 +0200

    Update docstring of `FileProduct` (#380)

--- src/Products.jl
+++ src/Products.jl
@@ -440,10 +440,11 @@ function locate(ep::ExecutableProduct, prefix::Prefix; platform::AbstractPlatfor
 end
 
 """
-    FileProduct(path::AbstractString, varname::Symbol, dir_path = nothing)
+    FileProduct(path, varname::Symbol, dir_path = nothing)
 
 Declares a [`FileProduct`](@ref) that points to a file located relative to the root of
-a `Prefix`, must simply exist to be satisfied.
+a `Prefix`, must simply exist to be satisfied. The first argument `path` is either a single `AbstractString` 
+or a `Vector{String}` to allow several paths to be checked.   
 """
 struct FileProduct <: Product
     paths::Vector{String}
