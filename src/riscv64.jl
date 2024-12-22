99731595a43b2d37964287001294ccab2937088e
Erik Schnetter <schnetter@gmail.com>
2024-12-21 20:09:37 -0500

    More riscv64 support

--- src/riscv64.jl
+++ src/riscv64.jl
@@ -6,11 +6,20 @@ using Base: BinaryPlatforms
 # These changes have been upstreamed to newer Julia versions, but we are stuck with Julia 1.7.
 # This is not pretty. It seems to work.
 
-using .BinaryPlatforms: CPUID
-CPUID.ISAs_by_family["riscv64"] = [
-    # We have no way to test riscv64 features yet, so we're only going to declare the lowest ISA:
-    "riscv64" => CPUID.ISA(Set{UInt32}()),
-]
+function __init__()
+    CPUID.ISAs_by_family["riscv64"] = [
+        # We have no way to test riscv64 features yet, so we're only going to declare the lowest ISA:
+        "riscv64" => CPUID.ISA(Set{UInt32}()),
+    ]
+
+    BinaryPlatforms.arch_mapping["riscv64"] = "(rv64|riscv64)"
+
+    function get_set(arch, name)
+        all = BinaryPlatforms.CPUID.ISAs_by_family[arch]
+        return all[findfirst(x -> x.first == name, all)].second
+    end
+    BinaryPlatforms.arch_march_isa_mapping["riscv64"] = ["riscv64" => get_set("riscv64", "riscv64")]
+end
 
 function Base.BinaryPlatforms.validate_tags(tags::Dict)
     throw_invalid_key(k) = throw(ArgumentError("Key \"$(k)\" cannot have value \"$(tags[k])\""))
@@ -76,116 +85,108 @@ function Base.BinaryPlatforms.validate_tags(tags::Dict)
     end
 end
 
-using .BinaryPlatforms: arch_mapping, os_mapping, libc_mapping, call_abi_mapping,
-    libgfortran_version_mapping, cxxstring_abi_mapping, libstdcxx_version_mapping
-
-arch_mapping["riscv64"] = "(rv64|riscv64)"
-
-function get_set(arch, name)
-    all = BinaryPlatforms.CPUID.ISAs_by_family[arch]
-    return all[findfirst(x -> x.first == name, all)].second
-end
-BinaryPlatforms.arch_march_isa_mapping["riscv64"] = ["riscv64" => get_set("riscv64", "riscv64")]
-
-function Base.parse(::Type{Platform}, triplet::AbstractString; validate_strict::Bool = false)
-    # Re-insert the architecture because the global assignments above don't stick
-    CPUID.ISAs_by_family["riscv64"] = [
-        # We have no way to test riscv64 features yet, so we're only going to declare the lowest ISA:
-        "riscv64" => CPUID.ISA(Set{UInt32}()),
-    ]
-    BinaryPlatforms.arch_mapping["riscv64"] = "(rv64|riscv64)"
-    BinaryPlatforms.arch_march_isa_mapping["riscv64"] = ["riscv64" => get_set("riscv64", "riscv64")]
-
-    # Helper function to collapse dictionary of mappings down into a regex of
-    # named capture groups joined by "|" operators
-    c(mapping) = string("(",join(["(?<$k>$v)" for (k, v) in mapping], "|"), ")")
-
-    # We're going to build a mondo regex here to parse everything:
-    triplet_regex = Regex(string(
-        "^",
-        # First, the core triplet; arch/os/libc/call_abi
-        c(arch_mapping),
-        c(os_mapping),
-        c(libc_mapping),
-        c(call_abi_mapping),
-        # Next, optional things, like libgfortran/libstdcxx/cxxstring abi
-        c(libgfortran_version_mapping),
-        c(cxxstring_abi_mapping),
-        c(libstdcxx_version_mapping),
-        # Finally, the catch-all for extended tags
-        "(?<tags>(?:-[^-]+\\+[^-]+)*)?",
-        "\$",
-    ))
-
-    m = match(triplet_regex, triplet)
-    if m !== nothing
-        # Helper function to find the single named field within the giant regex
-        # that is not `nothing` for each mapping we give it.
-        get_field(m, mapping) = begin
-            for k in keys(mapping)
-                if m[k] !== nothing
-                    # Convert our sentinel `nothing` values to actual `nothing`
-                    if endswith(k, "_nothing")
-                        return nothing
-                    end
-                    # Convert libgfortran/libstdcxx version numbers
-                    if startswith(k, "libgfortran")
-                        return VersionNumber(parse(Int,k[12:end]))
-                    elseif startswith(k, "libstdcxx")
-                        return VersionNumber(3, 4, parse(Int,m[k][11:end]))
-                    else
-                        return k
-                    end
-                end
-            end
-        end
-
-        # Extract the information we're interested in:
-        arch = get_field(m, arch_mapping)
-        os = get_field(m, os_mapping)
-        libc = get_field(m, libc_mapping)
-        call_abi = get_field(m, call_abi_mapping)
-        libgfortran_version = get_field(m, libgfortran_version_mapping)
-        libstdcxx_version = get_field(m, libstdcxx_version_mapping)
-        cxxstring_abi = get_field(m, cxxstring_abi_mapping)
-        function split_tags(tagstr)
-            tag_fields = filter(!isempty, split(tagstr, "-"))
-            if isempty(tag_fields)
-                return Pair{String,String}[]
-            end
-            return map(v -> Symbol(v[1]) => v[2], split.(tag_fields, "+"))
-        end
-        tags = split_tags(m["tags"])
-
-        # Special parsing of os version number, if any exists
-        function extract_os_version(os_name, pattern)
-            m_osvn = match(pattern, m[os_name])
-            if m_osvn !== nothing
-                return VersionNumber(m_osvn.captures[1])
-            end
-            return nothing
-        end
-        os_version = nothing
-        if os == "macos"
-            os_version = extract_os_version("macos", r".*darwin([\d\.]+)")
-        end
-        if os == "freebsd"
-            os_version = extract_os_version("freebsd", r".*freebsd([\d.]+)")
-        end
-
-        return Platform(
-            arch, os;
-            validate_strict,
-            libc,
-            call_abi,
-            libgfortran_version,
-            cxxstring_abi,
-            libstdcxx_version,
-            os_version,
-            tags...,
-        )
-    end
-    throw(ArgumentError("Platform `$(triplet)` is not an officially supported platform"))
-end
+#TODO using .BinaryPlatforms: arch_mapping, os_mapping, libc_mapping, call_abi_mapping,
+#TODO     libgfortran_version_mapping, cxxstring_abi_mapping, libstdcxx_version_mapping
+#TODO 
+#TODO function Base.parse(::Type{Platform}, triplet::AbstractString; validate_strict::Bool = false)
+#TODO     # Re-insert the architecture because the global assignments above don't stick
+#TODO     CPUID.ISAs_by_family["riscv64"] = [
+#TODO         # We have no way to test riscv64 features yet, so we're only going to declare the lowest ISA:
+#TODO         "riscv64" => CPUID.ISA(Set{UInt32}()),
+#TODO     ]
+#TODO     BinaryPlatforms.arch_mapping["riscv64"] = "(rv64|riscv64)"
+#TODO     BinaryPlatforms.arch_march_isa_mapping["riscv64"] = ["riscv64" => get_set("riscv64", "riscv64")]
+#TODO 
+#TODO     # Helper function to collapse dictionary of mappings down into a regex of
+#TODO     # named capture groups joined by "|" operators
+#TODO     c(mapping) = string("(",join(["(?<$k>$v)" for (k, v) in mapping], "|"), ")")
+#TODO 
+#TODO     # We're going to build a mondo regex here to parse everything:
+#TODO     triplet_regex = Regex(string(
+#TODO         "^",
+#TODO         # First, the core triplet; arch/os/libc/call_abi
+#TODO         c(arch_mapping),
+#TODO         c(os_mapping),
+#TODO         c(libc_mapping),
+#TODO         c(call_abi_mapping),
+#TODO         # Next, optional things, like libgfortran/libstdcxx/cxxstring abi
+#TODO         c(libgfortran_version_mapping),
+#TODO         c(cxxstring_abi_mapping),
+#TODO         c(libstdcxx_version_mapping),
+#TODO         # Finally, the catch-all for extended tags
+#TODO         "(?<tags>(?:-[^-]+\\+[^-]+)*)?",
+#TODO         "\$",
+#TODO     ))
+#TODO 
+#TODO     m = match(triplet_regex, triplet)
+#TODO     if m !== nothing
+#TODO         # Helper function to find the single named field within the giant regex
+#TODO         # that is not `nothing` for each mapping we give it.
+#TODO         get_field(m, mapping) = begin
+#TODO             for k in keys(mapping)
+#TODO                 if m[k] !== nothing
+#TODO                     # Convert our sentinel `nothing` values to actual `nothing`
+#TODO                     if endswith(k, "_nothing")
+#TODO                         return nothing
+#TODO                     end
+#TODO                     # Convert libgfortran/libstdcxx version numbers
+#TODO                     if startswith(k, "libgfortran")
+#TODO                         return VersionNumber(parse(Int,k[12:end]))
+#TODO                     elseif startswith(k, "libstdcxx")
+#TODO                         return VersionNumber(3, 4, parse(Int,m[k][11:end]))
+#TODO                     else
+#TODO                         return k
+#TODO                     end
+#TODO                 end
+#TODO             end
+#TODO         end
+#TODO 
+#TODO         # Extract the information we're interested in:
+#TODO         arch = get_field(m, arch_mapping)
+#TODO         os = get_field(m, os_mapping)
+#TODO         libc = get_field(m, libc_mapping)
+#TODO         call_abi = get_field(m, call_abi_mapping)
+#TODO         libgfortran_version = get_field(m, libgfortran_version_mapping)
+#TODO         libstdcxx_version = get_field(m, libstdcxx_version_mapping)
+#TODO         cxxstring_abi = get_field(m, cxxstring_abi_mapping)
+#TODO         function split_tags(tagstr)
+#TODO             tag_fields = filter(!isempty, split(tagstr, "-"))
+#TODO             if isempty(tag_fields)
+#TODO                 return Pair{String,String}[]
+#TODO             end
+#TODO             return map(v -> Symbol(v[1]) => v[2], split.(tag_fields, "+"))
+#TODO         end
+#TODO         tags = split_tags(m["tags"])
+#TODO 
+#TODO         # Special parsing of os version number, if any exists
+#TODO         function extract_os_version(os_name, pattern)
+#TODO             m_osvn = match(pattern, m[os_name])
+#TODO             if m_osvn !== nothing
+#TODO                 return VersionNumber(m_osvn.captures[1])
+#TODO             end
+#TODO             return nothing
+#TODO         end
+#TODO         os_version = nothing
+#TODO         if os == "macos"
+#TODO             os_version = extract_os_version("macos", r".*darwin([\d\.]+)")
+#TODO         end
+#TODO         if os == "freebsd"
+#TODO             os_version = extract_os_version("freebsd", r".*freebsd([\d.]+)")
+#TODO         end
+#TODO 
+#TODO         return Platform(
+#TODO             arch, os;
+#TODO             validate_strict,
+#TODO             libc,
+#TODO             call_abi,
+#TODO             libgfortran_version,
+#TODO             cxxstring_abi,
+#TODO             libstdcxx_version,
+#TODO             os_version,
+#TODO             tags...,
+#TODO         )
+#TODO     end
+#TODO     throw(ArgumentError("Platform `$(triplet)` is not an officially supported platform"))
+#TODO end
 
 end
