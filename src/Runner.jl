95fa38e384a269260f9d299b4146b2ee93419ea3
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-04-08 02:16:55 +0200

    use double-quotes for interpolation, avoid backticks

--- src/Runner.jl
+++ src/Runner.jl
@@ -744,12 +744,12 @@ function generate_compiler_wrappers!(platform::AbstractPlatform; bin_path::Abstr
         extra_cmds = """
         if [[ " \${ARGS[@]} " == *'--target'* ]]; then
             if ! [[ " \${ARGS[@]} " =~ --target(=| )\${CARGO_BUILD_TARGET} ]]; then
-                echo "Attempting to invoke targeted 'rustc' wrapper with a different target! (Expected \${CARGO_BUILD_TARGET}, which is `CARGO_BUILD_TARGET`)" >&2
+                echo "Attempting to invoke targeted 'rustc' wrapper with a different target! (Expected \${CARGO_BUILD_TARGET}, which is CARGO_BUILD_TARGET)" >&2
                 echo "args: \${ARGS[@]}" >&2
                 exit 1
             fi
         else
-            PRE_FLAGS+=( '--target=\${CARGO_BUILD_TARGET}' )
+            PRE_FLAGS+=( "--target=\${CARGO_BUILD_TARGET}" )
         fi
         """
         wrapper(io, "/opt/$(host_target)/bin/rustc"; flags=rust_flags!(p), allow_ccache=false, extra_cmds=extra_cmds)
