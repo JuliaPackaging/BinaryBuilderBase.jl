2dadeb4620792aa74f4bff19e87ad6078f68a2e4
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-09-04 20:20:38 +0100

    [Runner] Fix condition under which the ccache volume is mounted

--- src/Runner.jl
+++ src/Runner.jl
@@ -1573,7 +1573,7 @@ function runner_setup!(workspaces, mappings, workspace_root, verbose, kwargs, pl
         if !isdir(ccache_dir())
             mkpath(ccache_dir())
         end
-        if haskey(ENV, "CCACHE_DIR")
+        if haskey(envs, "CCACHE_DIR")
             # When bootstrapping, `CCACHE_DIR` is not defined.
             push!(workspaces, ccache_dir() => envs["CCACHE_DIR"])
         end
